1e13447d6eef737f5115200e187a5aca4b80cd54
katsuya <>
2017-11-15 06:17:54 +0900

    Add basic authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic authenticate_with :name => "katsuya", :password => "kA141913" if Rails.env == "production"
   protect_from_forgery with: :exception
   before_action :authenticate_user!, only: [:after_sign_in_path_for]
 
