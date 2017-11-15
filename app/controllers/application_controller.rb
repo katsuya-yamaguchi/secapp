ca39066d1b8574b08bcc1f239d3f3100e898b43a
katsuya <>
2017-11-15 19:59:30 +0900

    Add basic authenticate.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, only: [:after_sign_in_path_for]
-  before :basic
+  before_filter :basic
 
   def after_sign_in_path_for(resource)
     admin_path
