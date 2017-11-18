c85cb34b86b4f68f8c2ffa5bc75ac89c5180d393
katsuya <katsuya@yamaguchikatsuya-no-MacBook-Pro.local>
2017-11-18 19:43:55 +0900

    [modify] basic認証処理の削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, only: [:after_sign_in_path_for]
-  before_filter :basic
 
   def after_sign_in_path_for(resource)
     admin_path
-  end
-
-  private
-  def basic
-    authenticate_or_request_with_http_basic do |name, password| 
-      name == "katsuya" && password == "kA141913"
-    end
   end
 end
