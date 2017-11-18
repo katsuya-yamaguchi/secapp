57a7e1ce0695dba9526bf76da4ab2964c6282d32
katsuya <katsuya@yamaguchikatsuya-no-MacBook-Pro.local>
2017-11-18 23:48:22 +0900

    [add] Get admin_pathした際のテストを作成

--- spec/requests/management_spec.rb
+++ spec/requests/management_spec.rb
@@ -2,12 +2,31 @@ require 'rails_helper'
 
 describe "management" do
   let(:user){ create(:user) }
+  let(:video_group){ create(:video_group) }
+  let(:login_to_get){
+    login_as(user)
+    get admin_path
+  }
   context "Get admin_path" do
-    it "Response is 200." do
-      login_as(user)
+    it "Redirect to root_path." do
       get admin_path
+      expect(response).to redirect_to("/")
+    end
+    it "Response is 200." do
+      login_to_get
       expect(response).to have_http_status(200) 
     end
+    it "Successful new of Video model." do
+      login_to_get
+      expect(Video.new.attributes.length).to eq 11
+    end
+    it "Successful acquisition of Video groups." do
+      login_to_get
+      video_group
+      groups = [video_group.id, video_group.uq_group_name]
+      expect(groups).not_to be_empty
+    end
+    
   end
 
   context "Post admin_path" do
