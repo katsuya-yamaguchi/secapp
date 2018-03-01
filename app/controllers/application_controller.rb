ee4c378872d79718ee848de4aa10fcfaa31bf51b
katsuya <katsuya@yamaguchikatsuya-no-MacBook-Pro.local>
2018-03-02 06:37:13 +0900

    [update] 軽微な修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   private
   def sign_in_required
-    redirect_to new_user_session unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
