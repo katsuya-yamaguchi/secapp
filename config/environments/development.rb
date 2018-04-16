d61983cd32ddbe568b4d58085c5d5a8dbbcb7734
katsuya <katsuya@yamaguchikatsuya-no-MacBook-Pro.local>
2018-04-16 20:28:41 +0900

    [modify] メールアドレスの変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: "587",
-    user_name: "bb.win.3@gmail.com",
-    password: "ujcrzstnfnbmmtpg",
+    user_name: "shirobuhi.customer@gmail.com",
+    password: "spfkkibrxtznvcyk",
     authentication: :plain,
     enable_starttls_auto: true
   }
