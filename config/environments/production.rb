1d9559550a577cfd1c929b56f346895066d5ff7a
katsuya <katsuya@yamaguchikatsuya-no-MacBook-Pro.local>
2018-04-17 21:04:18 +0900

    [fix] 各種修正
    メールアドレスのパスワード修正。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: "587",
     user_name: "shirobuhi.customer@gmail.com",
-    password: "uvjnmezgjfjonlyt",
+    password: "spfkkibrxtznvcyk",
     authentication: :plain,
     enable_starttls_auto: true
   }
--- config/routes.rb
+++ config/routes.rb
@@ -15,20 +15,13 @@ Rails.application.routes.draw do
   get '/delete/:id', to: 'static_pages#delete_user', as: 'delete_user'
 
   get '/video/:id', to: 'static_pages#video'
+  delete '/video/:id', to: 'static_pages#video_destroy'
 
   resources :videos do
     resources :likes, only: [:create, :destroy]
   end
 
   get '/riyokiyaku', to: 'static_pages#riyokiyaku'
-  get '/courses', to: 'static_pages#courses'
-  get '/courses/:id', to: 'static_pages#list_courses'
-  get '/courses/:id/video', to: 'static_pages#list_videos'
-  get '/courses/:id/video/:video_perm', to: 'static_pages#videos'
-  
-  get '/admins', to: 'admins#index'
-  post '/admins', to: 'admins#file_upload'
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
